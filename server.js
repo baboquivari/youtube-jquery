4132ffdd13f8d3fbf20b6a70dd184bcfe7c257bf
unknown <to.sian.san@gmail.com>
2017-08-07 12:40:26 +0100

    herokue time

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, 'public')));
+app.use(express.static(path.resolve(__dirname)));
 
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(process.env.PORT || 9090, () => {
